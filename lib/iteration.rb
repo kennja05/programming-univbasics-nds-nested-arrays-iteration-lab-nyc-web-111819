f14ef2391063b52c6a0783b5e8df23798a6c6380
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-13 16:21:31 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -52,10 +52,10 @@ total = 0
 row_index = 0 
 while row_index < src.count do 
   element_index = 0
-  
+  inner_total
   while element_index < (src[row_index].count) do 
     if (src[row_index][0] % 2) + (src[row_index][1] % 2) == 0 
-      total = (src[row_index][0] + src[row_index][1])
+      inner_total = (src[row_index][0] + src[row_index][1])
     end
   element_index+=1   
   end
